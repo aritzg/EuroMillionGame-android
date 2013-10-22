abf627a979e3edfbbda1a90847f353935217e1a4
Aritz Galdos <galdos.aritz@gmail.com>
2013-10-22 23:31:26 +0200

    Just created. Good luck

---
+++ src/main/java/net/sareweb/android/emg/EuroMillionGameActivity.java
@@ -0,0 +1,26 @@
+
+package net.sareweb.android.emg;
+
+import android.widget.TextView;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class EuroMillionGameActivity
+    extends SherlockActivity
+{
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
